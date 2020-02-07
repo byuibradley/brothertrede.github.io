62c276d73ea645aa3832427b9b1330584c69f952
byuibradley <btrede@byui.edu>
2020-02-06 21:47:45 -0700

    updates

---
+++ Lesson5/js/script.js
@@ -0,0 +1,42 @@
+function toggleMenu() {
+    document.getElementById("navBar").classList.toggle("hide");
+}
+
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = d.getDay();
+const monthName = d.getMonth();
+const year = d.getFullYear();
+const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+document.getElementById("currentdate").textContent = fulldate;
+
+var Now = new Date();
+var CurrentDay = Now.getDay();
+if ((CurrentDay == 0 && Now.getHours()>12) || CurrentDay == 4 ) {
+$('#announcement').show();
+} 
+else $('#noAnnouncement').show();
+
