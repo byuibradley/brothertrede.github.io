9ca68906729d53fdb92e282a882b491031d27a47
byuibradley <btrede@byui.edu>
2020-02-22 15:19:57 -0700

    lesson 7

---
+++ Lesson7/js/date_scripts.js
@@ -0,0 +1,40 @@
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+document.getElementById("currentdate").textContent = fulldate;
+
+var Now = new Date();
+var CurrentDay = Now.getDay();
+if (CurrentDay == 0 || CurrentDay == 1 || CurrentDay == 2 || CurrentDay == 3 || CurrentDay == 4 || CurrentDay == 6)
+
+$('#noAnnouncement').show();
+
+else $('#announcement').show();
