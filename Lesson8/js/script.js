86cd2f1d83da7d34e07e0018485f17f948dcc682
byuibradley <btrede@byui.edu>
2020-02-27 19:21:01 -0700

    lesson8

---
+++ Lesson8/js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    document.getElementById("navBar").classList.toggle("hide");
+}
+
+function StormSeverity(severity) {
+    document.getElementById("getSeverity").innerHTML = severity;
+}
+
+
